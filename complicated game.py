d9921112eb4913e521381172246df2a8262bab59
Pripro123 <40630891+Pripro123@users.noreply.github.com>
2018-06-27 16:36:05 +0100

    Add files via upload

--- complicated game.py
+++ complicated game.py
@@ -44,6 +44,22 @@ Matrix[5][5] = "The monster spots you as you step on a twig accidently- now what
 
 Matrix[5][3] = "Your leg steps on its tail and its eyes open"
 
+Matrix[2][5] = "You're sprinting away but as you run you feel like something is watching you from the trees"
+
+Matrix[5][2] = "It screeches as you have realised that you have only angered it further, you sprint away only to feel as if something is watching you from the trees"
+
+Matrix[2][6] = "Oh no, you realise that the fire you started has created a way for the monster to find you!"
+
+Matrix[6][2] = "The fire is big and smoke rises and then it hits you- it's a way for the creature to find you"
+
+Matrix[2][7] = "As you follow the footprints, you realise that it was a trap as you fall into a bear trap- now what you wonder as you hear the creature approaching"
+
+Matrix[7][2] = "You step into a bear trap and are stuck with a look of horror on your face as you hear the creature approaching"
+
+Matrix[3][7] = "Game over"
+
+Matrix[7][3] = "Game over"
+
 
 choice1 = int( input ("Should you 1 - Go north? 2 - Go east?"))
 
@@ -114,6 +130,81 @@ print("The sun is setting and you must run in any direction. Make the wrong choi
 
 
 print()
+
+print("It slowly creeps towards you and you take a deep breathe in")
+
+choice5 = int(input("You can either 1- Run! or 2- Hit it with a sharp pebble that is on the floor next to you"))
+
+if choice5 == 1:
+
+    print(Matrix[2][5])
+
+else:
+
+    print(Matrix[5][2])
+
+print()
+
+choice6 = int(input ("You find shelter and start a fire to cook 1- Fish or 2- Coconut"))
+
+if choice6 == 1:
+
+    print(Matrix[2][6])
+
+else:
+
+    print(Matrix[6][2])
+
+print()
+
+print("You need to run, however you are tired and want to confront the monster")
+
+print()
+
+print("You decide to kill the beast.")
+
+print()
+
+choice7 = int(input ("Should you 1- Follow the beast's large footprints? 2- Follow the trail back to the cave you ran away from"))
+
+if choice7 == 1:
+
+    print(Matrix[2][7])
+
+else:
+
+    print(Matrix[7][2])
+
+print()
+
+print("You have to make a decision")
+
+choice8 = int(input ("Should you 1- Look like bait and kill the beast when it thinks you are unconcious or 2- Cute yourself loose and hide behind a tree,ready to kill the creature"))
+
+if choice8 ==1:
+    
+
+    print(Matrix[3][7])
+
+else:
+    
+    print(Matrix[7][3])
+
+print()
+
+print("Your plan failed, it turns out that you were right. There was something in the trees there was a herd of creatures that you couldn't escape...")
+
+
+    
+
+
+
+
+              
+
+
+
+              
 
 
 
