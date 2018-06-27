5546035fb78448794383644f53f023ff67b34bc8
Pripro123 <40630891+Pripro123@users.noreply.github.com>
2018-06-27 14:07:48 +0100

    Add files via upload

---
+++ complicated game.py
@@ -0,0 +1,147 @@
+
+print ("You find yourself trapped alone on an island")
+
+print()
+
+print("You must escape and find supplies in order to escape before it is too late")
+
+print()
+
+print ("You have nothing but a map and must decide where to explore before it reaches dark...")
+
+
+commands = ["North","South","East","West","Get","Use"]
+
+print ( "You are coordinates 1,1 on your map.")
+
+print()
+
+print ("Which direction would you like to go?")
+
+
+X, Y = 8, 8;
+Matrix = [[0 for x in range(X)] for y in range(X)]
+
+Matrix[1][1] = "You are at Sea"
+
+Matrix[1][2] = "You reach a secret compound that requires a key... You must explore more."
+
+Matrix[2][1] = "Another endless piece of grassland, hurry as daylight it running out."
+
+Matrix[2][2] = "You have reached the coast and see a petrifying monster and realise that you must run."                    
+
+Matrix[3][2] = "You sigh of relief as you have discovered food, however you see something awful approaching and must run"
+
+Matrix[3][3] = "You look over a cliff and see a huge and terrifying monster approaching, you need to run."
+
+Matrix[2][3] = "You find a shimmering key and realise that it is your way home, it then goes pitch black, the monster got you..."
+
+Matrix[4][2] = "You reach for a bronze key and then everything goes black- the creature hit you..."
+
+Matrix[2][4] = "You reach for a silver key, it goes black- the monster got to you..."
+
+Matrix[5][5] = "The monster spots you as you step on a twig accidently- now what?!"
+
+Matrix[5][3] = "Your leg steps on its tail and its eyes open"
+
+
+choice1 = int( input ("Should you 1 - Go north? 2 - Go east?"))
+
+if choice1 == 1:
+    
+    print (Matrix[1][2])
+
+       
+elif choice1 == 2:
+
+    print (Matrix[2][1])
+
+print ("Where to now?")
+
+choice2 = int( input ("Should you 1 - Go north? 2 -Go east? 3 -Go west?"))
+               
+               
+if choice2 == 1:
+
+    print(Matrix[2][2])
+               
+               
+elif choice2 == 2:
+
+    print(Matrix[3][2])
+               
+else:
+    print(Matrix[3][3])
+
+
+print (" You need to decide now where to go next!")
+
+choice3 = int( input ("Run 1- north 2- east 3- west"))
+               
+if choice3 == 1:
+
+    print(Matrix[2][3])
+    
+elif choice3 ==2:
+
+    print(Matrix[4][2])
+
+else:
+
+    print(Matrix[2][4])
+
+print()
+
+print("You wake up to find yourself on the muddy floor in a cave with a shadow looming above you")
+
+print()
+
+print("What should you do. Choose carefully as it could lead to game over...")
+
+print()
+
+choice4 = int(input ("You can either 1- explore the light to the left of you 2- Tip toe over the monster's body as it sleeps"))
+
+if choice4 == 1:
+
+    print(Matrix[5][5])
+
+else:
+
+    print(Matrix[5][3])
+
+print("The sun is setting and you must run in any direction. Make the wrong choice and you won't have enough supplies to survive...")
+
+
+print()
+
+
+
+
+
+
+
+    
+
+              
+
+
+
+    
+        
+
+
+               
+
+               
+               
+               
+
+    
+
+       
+
+
+
+
+
